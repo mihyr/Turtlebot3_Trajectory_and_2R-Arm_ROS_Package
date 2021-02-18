cc0acbc8b646a45c999d61e143784b90a7a4b8a5
whomihirpatel <mspatel1298@gmail.com>
2021-02-18 16:15:06 -0600

    Update (read more):     updated trajectorymath.py and armmath.py with sphinx comments     updated all nodes with comments     modified figure8 launch file (removed xarco), now uses turtlebot3_bringup xarco     updated rviz config, added axis and modified turtle_view

--- src/homework2/trajectorymath.py
+++ src/homework2/trajectorymath.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python
-from math import sin, cos, pi, atan2
+from math import sin, cos, pi, atan
 from numpy import arctan, sqrt
 
 def traj_coordinates(h,w,current_time,total_time):
+    '''Calculates figure 8 trajectory coordinates
+    Args:
+        h - height of figure 8
+        w - width of figure 8
+        current_time - current time in ROS
+        total_time - time required to complete one complete figure 8 loop
+    Return:
+        x,y - x and y coordinates of figure 8 trajectory
+    '''
+    #Trajectory equation
     x = (w/2)*sin(2*pi*current_time/total_time)
     y = (h/2)*sin(4*pi*current_time/total_time)
+    
     return x,y
 
 def xy_derivatives(x,y,h,w,t,T):
+    '''Calculates first and second order derivative of x and y and returns linear and angular velocity for figure 8 trajectory
+    Args:
+        x_dot, y_dot - first order derivative of x and y coordinates
+        x_dd, y_dd - second order derivative of x and y coordinates
+        h - Height of figure 8
+        w - Width of figure 8
+        t - current time in ROS
+        T -time required to complete one complete figure 8 loop
+    Return:
+        Velocity: linear velocity (Vx)
+        omega: Angular velocity (Y)
+        theta: angle theta, used for calculating quat in world to odom transformation
+
+    '''
     x_dot = (w*pi/T)*cos(2*pi*t/T)
     y_dot = (2*h*pi/T)*cos(4*pi*t/T)
 
@@ -16,6 +41,6 @@ def xy_derivatives(x,y,h,w,t,T):
     
     velocity = sqrt(x_dot**2 + y_dot**2)
     omega = ((y_dd*x_dot)-(y_dot*x_dd))/((x_dot**2+y_dot**2))
-    theta = atan2(y_dot,x_dot)
+    theta = atan(y_dot/x_dot)
 
-    return velocity, omega, theta
+    return velocity, omega, theta   
